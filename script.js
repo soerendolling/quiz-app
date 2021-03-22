047932e446eb40a664c0ffab90d3c1157b918e94
Soeren Dolling <soeren.dolling@gmail.com>
2021-03-22 19:47:37 +0100

    Toogle bookmark

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const content = document.querySelector(".main-grid__container")
 for (let i = 0; i < data.length;i++) {
    const cardArticle = createCard(data[i])
    content.append(cardArticle)
+
 }
 
 
@@ -28,15 +29,27 @@ button[i].addEventListener('click', () => {
  
 const bookmark = document.querySelectorAll(".quiz-card__bookmark")
 for (let i = 0; i < bookmark.length;i++){
+
+   if (data[i].isBookmarked) {
+      bookmark[i].style.fill = "black"
+   } else  {
+      bookmark[i].style.fill = "none"
+   }
+
+
+
     bookmark[i].addEventListener('click', () => {
      const bookmarkTicked = bookmark[i].classList.toggle(".quiz-card__bookmark")
      
-     if (bookmarkTicked) {
+     console.log(bookmarkTicked)
+     console.log(data[i].isBookmarked)
+
+     if (bookmarkTicked)  {
         bookmark[i].style.fill = "black"
-        console.log("notticked")
-     } else {
-        bookmark[i].style.fill = "none"
-        console.log("ticked")
+        console.log("filled")
+     } else  {
+      bookmark[i].style.fill = "none"
+      console.log("notfilled")
      }
     
     });
@@ -66,36 +79,36 @@ const inputQuestion = form["question__text"];
 })
 
 
-function myFunction() {
-   console.log("button-pressed")
-   const element = document.querySelector(".body-grid__main");
-   const footer = document.querySelector(".body-grid__footer")
-   const dmButton = document.querySelector(".dark-mode__button");
-   const tags = document.querySelectorAll(".profile-skill__tag");
-   const loButton = document.querySelector(".logout__button");
-   const header = document.querySelector(".body-grid__header")
-   const text = document.querySelector(".profile-text")
-   const profile = document.querySelector(".picture-section__picture")
-   const name = document.querySelector(".picture-section__name")
+// function myFunction() {
+//    console.log("button-pressed")
+//    const element = document.querySelector(".body-grid__main");
+//    const footer = document.querySelector(".body-grid__footer")
+//    const dmButton = document.querySelector(".dark-mode__button");
+//    const tags = document.querySelectorAll(".profile-skill__tag");
+//    const loButton = document.querySelector(".logout__button");
+//    const header = document.querySelector(".body-grid__header")
+//    const text = document.querySelector(".profile-text")
+//    const profile = document.querySelector(".picture-section__picture")
+//    const name = document.querySelector(".picture-section__name")
 
-   for (let i = 0; i < tags.length;i++){
-      tags[i].style.backgroundColor = "pink"
-   tags[i].style.color = "white"
-   }
+//    for (let i = 0; i < tags.length;i++){
+//       tags[i].style.backgroundColor = "pink"
+//    tags[i].style.color = "white"
+//    }
 
-   header.style.backgroundColor = "darkgrey"
-   element.style.backgroundColor = "black"
-   footer.style.backgroundColor = "darkgrey"
-   dmButton.style.backgroundColor = "pink"
-   dmButton.style.color = "white"
-   loButton.style.backgroundColor = "pink"
-   loButton.style.color = "white"
-   text.style.backgroundColor = "pink"
-   text.style.color = "white"
-   profile.style.backgroundColor = "pink"
-   name.style.backgroundColor = "pink"
-   name.style.color = "white"
-}
+//    header.style.backgroundColor = "darkgrey"
+//    element.style.backgroundColor = "black"
+//    footer.style.backgroundColor = "darkgrey"
+//    dmButton.style.backgroundColor = "pink"
+//    dmButton.style.color = "white"
+//    loButton.style.backgroundColor = "pink"
+//    loButton.style.color = "white"
+//    text.style.backgroundColor = "pink"
+//    text.style.color = "white"
+//    profile.style.backgroundColor = "pink"
+//    name.style.backgroundColor = "pink"
+//    name.style.color = "white"
+// }
 
 
 
